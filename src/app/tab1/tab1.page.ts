6471cff0411ae74beee51cba8682408ae1b09f62
anhdaovan <daoanh08091999@gmail.com>
2024-06-05 16:52:21 +0700

    test alert

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  public alertBtn = {
+    text: 'OK',
+    role: 'confirm',
+    handler: () => {
+      console.log('Alert confirmed');
+    },
+  };
+  constructor(private alertController: AlertController) {}
+  async presentAlert() {
+    const alert = await this.alertController.create({
+      header: 'A Short Title Is Best',
+      message: 'A message should be a short, complete sentence.',
+      buttons: [this.alertBtn],
+    });
 
-  constructor() {}
-
+    await alert.present();
+  }
 }
